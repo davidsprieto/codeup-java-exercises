3cd8d2a6071a633ae8e0232e3246991c4b95e334
David Prieto <david.s.prieto1@gmail.com>
2022-05-20 15:45:22 -0500

    Complete #1 of Java II - Arrays Exercises.

---
+++ src/ArraysExercises.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+    // Method to add a new person to the 'people' array.
+    public static Person[] addPerson(Person[] people, Person personToAdd) {
+        people = Arrays.copyOf(people, people.length + 1);
+        people[people.length - 1] = personToAdd;
+        return people;
+    }
+
+    public static void main(String[] args) {
+        // #1 - Arrays Basics
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers); // Prints: [I@129a8472 - need to use Arrays.toString() in order to print the array.
+        System.out.println(Arrays.toString(numbers)); // Prints: [1, 2, 3, 4, 5]
+
+        Person[] people;
+        people = new Person[3]; // Can also be written like: Person[] people = new Person[3].
+        System.out.println(Arrays.toString(people)); // Prints: [null, null, null], because the elements in the array have no assigned values.
+        people[0] = new Person("Joe"); // Assigning a new instance of the Person class to each element (lines 21 - 23).
+        people[1] = new Person("Bob");
+        people[2] = new Person("Bill");
+        System.out.println(Arrays.toString(people)); // Prints: [Person@2f7c7260, Person@2d209079, Person@6bdf28bb]. Need to use .getName() method to print the names of each person in the people array. However, cannot chain that method on in this line of code.
+
+        // Iterating through the people array with a 'for' loop to print out the name of each person:
+        for (int i = 0; i < people.length; i++) {
+            System.out.println(people[i].getName()); // Prints: Joe, Bob, Bill on separate lines. Using .getName() method will get the name of the person(s) in the people array.
+        }
+
+        // Iterating through the people array with an enhanced 'for' loop to print out the name of each person:
+        for (Person person : people) {
+            System.out.println(person.getName()); // Also prints: Joe, Bob, Bill on separate lines.
+        }
+
+        Person John = new Person("John"); // Creating a new instance of person named John to pass into addPerson method.
+        people = addPerson(people, John); // A call to addPerson method while passing in the array of 'people' and 'John' to be added to that array. And assigning that return value to the 'people' array.
+        for (int a = 0; a < people.length; a++) {
+            System.out.println(people[a].getName()); // Prints: Joe, Bob, Bill, John on separate lines.
+        }
+
+    }
+
+}
--- src/Person.java
+++ src/Person.java
@@ -22,26 +22,26 @@ public class Person {
         // #1 - Understanding References
         Person person1 = new Person("John");
         Person person2 = new Person("John");
-        System.out.println(person1.name); // Prints John
-        System.out.println(person2.name); // Also, prints John
-        System.out.println(person1.getName()); // Again, prints John
-        System.out.println(person1.getName().equals(person2.getName())); // Prints true because person1 & person2 have the same value for name.
-        System.out.println(person1 == person2); // Prints false because you can't use "==" (comparison operator) to compare Strings since Strings are not data types. Have to use .equals() to compare Strings.
+        System.out.println(person1.name); // Prints: John
+        System.out.println(person2.name); // Also, prints: John
+        System.out.println(person1.getName()); // Again, prints: John
+        System.out.println(person1.getName().equals(person2.getName())); // Prints: true, because person1 & person2 have the same value for name.
+        System.out.println(person1 == person2); // Prints: false, because you can't use "==" (comparison operator) to compare Strings since Strings are not data types. Have to use .equals() to compare Strings.
 
         Person person3 = new Person("John");
         Person person4 = person3;
-        System.out.println(person3 == person4); // Prints true because the value of person4 was assigned the same value of person3.
+        System.out.println(person3 == person4); // Prints: true, because the value of person4 was assigned the same value of person3.
 
         Person person5 = new Person("John");
         Person person6 = person5;
-        System.out.println(person5.getName()); // Prints John
-        System.out.println(person6.getName()); // Also, prints John
+        System.out.println(person5.getName()); // Prints: John
+        System.out.println(person6.getName()); // Also, prints: John
         person6.setName("Jane"); // Setting the name of person6 from John to Jane
-        System.out.println(person5.getName()); // Prints Jane because the name was set to Jane and person5's value is equal to person6's value (line 36).
-        System.out.println(person6.getName()); // Also, prints Jane
-        System.out.println(person6.name); // Again, prints Jane
-        person1.sayHello(); // prints Hello, John! - Doesn't need to be wrapped in a System.out.println() as that method already print's to the console. So, just need to call the method.
-        person6.sayHello(); // prints Hello, Jane!
+        System.out.println(person5.getName()); // Prints: Jane, because the name was set to Jane and person5's value is equal to person6's value (line 36).
+        System.out.println(person6.getName()); // Also, prints: Jane
+        System.out.println(person6.name); // Again, prints: Jane
+        person1.sayHello(); //Prints: Hello, John! - Doesn't need to be wrapped in a System.out.println() as that method already print's to the console. So, just need to call the method.
+        person6.sayHello(); // Prints: Hello, Jane!
     }
 
 }
