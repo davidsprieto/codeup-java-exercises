4ab4c1ce9fd2035eee44f8b391b09f0a0e1736d8
David Prieto <david.s.prieto1@gmail.com>
2022-05-20 11:42:25 -0500

    Completed first three Java II - OOP Exercises.

---
+++ src/util/Input.java
@@ -0,0 +1,52 @@
+package util;
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+
+    public Input() {
+        System.out.print("Please type something: ");
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return this.scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        System.out.print("Enter Yes/No: ");
+        String input = this.getString();
+        return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
+    }
+
+   public int getInt(int min, int max) {
+        int number;
+        do {
+            System.out.printf("Please enter an integer between %d and %d: ", min, max);
+            number = scanner.nextInt();
+        } while(number < min || number > max);
+        return number;
+    }
+
+    public int getInt() {
+        System.out.print("Please enter an integer: ");
+        return this.scanner.nextInt();
+    }
+
+    public double getDouble(double min, double max) {
+        double number;
+        do {
+            System.out.printf("%nPlease enter a double (decimal number) between %.2f and %.2f: ", min, max);
+            number = scanner.nextDouble();
+        } while(number < min || number > max);
+        return number;
+    }
+
+    public double getDouble() {
+        System.out.print("Please enter a double (decimal number): ");
+        return this.scanner.nextDouble();
+    }
+
+
+}
---
+++ src/util/InputTest.java
@@ -0,0 +1,26 @@
+package util;
+
+    // #1 - Input Validation Class - Testing Input.java methods.
+
+public class InputTest {
+
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        System.out.println(input.getString()); // Prints what the user enters.
+
+        System.out.println(input.yesNo()); // If you type "yes" or 'y', prints true. If you type "no" or 'n', prints false.
+
+        System.out.println(input.getInt()); // Prints the integer the user enters.
+
+        System.out.println(input.getDouble()); // Prints the double (decimal number) the user enters.
+
+        System.out.print(input.getInt(1, 20)); // Prints the integer the user enters as long as it is between 1 and 20. It will continue to ask if it is outside the passed in ranges.
+
+        System.out.print(input.getDouble(1, 50)); // Prints the double (decimal number) the user enters as long as it is between 1 and 50. It will continue to ask if it is outside the passed in ranges.
+
+    }
+
+
+
+}
