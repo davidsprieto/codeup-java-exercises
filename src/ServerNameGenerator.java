a382d28dbb91bd1aeaa4c9d283ad58c98581bda2
David Prieto <david.s.prieto1@gmail.com>
2022-05-23 14:52:45 -0500

    Added comments.

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.Random; // Importing random number generator to use and store into a variable when needed.
 
 public class ServerNameGenerator {
     // #2 - Arrays: Server name generator
@@ -6,9 +6,9 @@ public class ServerNameGenerator {
     private static final String[] nouns = {"apple", "gold", "hamburger", "pizza", "potato", "breakfast", "leather", "whale", "football", "xylophone"}; // "final" keyword is like "const" in javascript.
 
     public static String randomName() {
-        int randomAdjectiveIndex = new Random().nextInt(10); // int variable to store random number
-        int randomNounIndex = new Random().nextInt(10);
-        return adjectives[randomAdjectiveIndex] + "-" + nouns[randomNounIndex];
+        int randomAdjectiveIndex = new Random().nextInt(10); // int variable to store random number.
+        int randomNounIndex = new Random().nextInt(10); // int variable to store another random number.
+        return adjectives[randomAdjectiveIndex] + "-" + nouns[randomNounIndex]; // The method returns a random String from the "adjectives" array and a random String from the "nouns" array. The Strings are chosen from a random index using new Random.nextInt() which is stored in two variables and passed into the brackets which indicates the index position.
     }
 
     public static void main(String[] args) {
