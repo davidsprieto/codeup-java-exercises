20add6eacdf51a5816142b6405fb7e2c2a6a784e
David Prieto <david.s.prieto1@gmail.com>
2022-05-23 14:53:13 -0500

    Started Java II - Arrays Exercise #3.

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,2 +1,31 @@
-package movies;public class Movie {
+package movies;
+
+// #3 - Java II - Arrays: Movie List
+public class Movie {
+    private String name;
+    private String category;
+
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getMovieName() {
+        return this.name;
+    }
+
+    public String getMovieCategory() {
+        return this.category;
+    }
+
+    public String setMovieName(String newName) {
+        name = newName;
+        return name;
+    }
+
+    public String setMovieCategory(String newCategory) {
+        category = newCategory;
+        return category;
+    }
+
 }
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,2 +1,26 @@
-package movies;public class MoviesApplication {
+package movies;
+import util.Input;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// #3 - Java II - Arrays: Movie List
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+        Input input = new Input(); // Allows access to the methods in the "Input" class under "util".
+        Movie[] allMovies = MoviesArray.findAll();
+
+        System.out.println("What would you like to do: \n 0 - exit \n 1 - view all movies \n 2 - view movies in the animated category \n 3 - view movies in the drama category \n 4 - view movies in the horror category \n 5 - view movies in the sci-fi category");
+        System.out.print("Enter your choice --> ");
+        int userDecision = input.getInt(0, 5);
+            if(userDecision == 0) {
+                System.out.println("Exit? Okay, that's cool.");
+            } else if(userDecision == 1) {
+                for(Movie movie : allMovies) {
+
+                }
+            }
+
+    }
+
 }
