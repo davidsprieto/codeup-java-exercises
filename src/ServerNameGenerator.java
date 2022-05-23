1f01bc2233c3ef391e44259dd58403618623dbee
David Prieto <david.s.prieto1@gmail.com>
2022-05-23 10:01:06 -0500

    Completed Exercise #2 for Java II - Arrays.

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class ServerNameGenerator {
+    // #2 - Arrays: Server name generator
+    private static final String[] adjectives = {"adventurous", "courageous", "dangerous", "exuberant", "outrageous", "prickly", "shiny", "victorious", "wandering", "zealous"};
+    private static final String[] nouns = {"apple", "gold", "hamburger", "pizza", "potato", "breakfast", "leather", "whale", "football", "xylophone"};
+
+    public static String randomName() {
+        int randomAdjectiveIndex = new Random().nextInt(10);
+        int randomNounIndex = new Random().nextInt(10);
+        return adjectives[randomAdjectiveIndex] + "-" + nouns[randomNounIndex];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Here is your server name: " + randomName());
+    }
+
+}
