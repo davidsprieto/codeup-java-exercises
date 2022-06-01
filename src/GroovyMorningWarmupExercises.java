f0df6cba9991dc0ab6cb165f8246736bdff34174
David Prieto <david.s.prieto1@gmail.com>
2022-06-01 09:18:01 -0500

    Added morning warmups for 06/01/22.

--- src/GroovyMorningWarmupExercises.java
+++ src/GroovyMorningWarmupExercises.java
@@ -1,8 +1,14 @@
 public class GroovyMorningWarmupExercises {
 
     public static void main(String[] args) {
+        // 06/01/22 - Create a function that takes the age in years and returns the age in days:
+        // example input: 65
+        // expected output: 23725
 
 
+        // 06/01/22 - Create a function that takes two numbers and a mathematical operator "+", "-", "/", "*" and will perform a calculation with the given numbers:
+        // example inputs: 4, '/', 2
+        // expected output: 2
 
     }
 
